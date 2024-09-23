5ec01076e052ee94048b40f1f21549648564a066
rohan-goyal16 <rohan.goyal@comprotechnologies.com>
2024-09-23 13:00:34 +0530

    Added test for eBook page

--- testAutomation_v1.0/pages/ExperienceApp/eBook.page.js
+++ testAutomation_v1.0/pages/ExperienceApp/eBook.page.js
@@ -5,6 +5,17 @@ var appShellPage = require("./appShell.page.js");
 
 module.exports = {
   cqa_ebook_evolve: selectorFile.css.ComproC1.eBook.cqa_ebook_evolve,
+  contentButton: selectorFile.css.ComproC1.eBook.contentButton,
+  toolsButton: selectorFile.css.ComproC1.eBook.toolsButton,
+  closeButton: selectorFile.css.ComproC1.eBook.closeButton,
+  homeButton: selectorFile.css.ComproC1.eBook.homeButton,
+  myNotes: selectorFile.css.ComproC1.eBook.myNotes,
+  cqaEbookEvolveDropdown:
+    selectorFile.css.ComproC1.eBook.cqaEbookEvolveDropdown,
+  cqaTestEbookOnlyAssets:
+    selectorFile.css.ComproC1.eBook.cqaTestEbookOnlyAssets,
+  notes: selectorFile.css.ComproC1.eBook.notes,
+  pageNumber: selectorFile.css.ComproC1.eBook.pageNumber,
 
   isInitialized: async function () {
     var res;
@@ -24,11 +35,229 @@ module.exports = {
         (await action.getElementCount(this.cqa_ebook_evolve)) > 0
           ? await action.getText(this.cqa_ebook_evolve)
           : null,
-      my_homework:
-        (await action.getElementCount(this.my_homework)) > 0
-          ? await action.getText(this.my_homework)
+    };
+    return obj;
+  },
+
+  getData_eTextToolBar: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      contentButton:
+        (await action.getElementCount(this.contentButton)) > 0
+          ? await action.getText(this.contentButton)
+          : null,
+      toolsButton:
+        (await action.getElementCount(this.toolsButton)) > 0
+          ? await action.getText(this.toolsButton)
+          : null,
+      homeButton:
+        (await action.getElementCount(this.homeButton)) > 0
+          ? await action.getText(this.homeButton)
+          : null,
+      pageNumber:
+        (await action.getElementCount(this.pageNumber)) > 0
+          ? await action.getText(this.pageNumber)
           : null,
     };
     return obj;
+  },
+
+  getData_ebookContents: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      closeButton:
+        (await action.getElementCount(this.closeButton)) > 0
+          ? await action.getText(this.closeButton)
+          : null,
+    };
+    return obj;
+  },
+
+  getData_ebookToolsNotes: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      myNotes:
+        (await action.getElementCount(this.myNotes)) > 0
+          ? await action.getText(this.myNotes)
+          : null,
+    };
+    return obj;
+  },
+
+  getData_ebookContent: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      cqaEbookEvolveDropdown:
+        (await action.getElementCount(this.cqaEbookEvolveDropdown)) > 0
+          ? await action.getText(this.cqaEbookEvolveDropdown)
+          : null,
+    };
+    return obj;
+  },
+
+  getData_ebookContentDropdown: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      cqaTestEbookOnlyAssets:
+        (await action.getElementCount(this.cqaTestEbookOnlyAssets)) > 0
+          ? await action.getText(this.cqaTestEbookOnlyAssets)
+          : null,
+    };
+    return obj;
+  },
+
+  getData_ebookTools: async function () {
+    await logger.logInto(await stackTrace.get());
+    var obj;
+    obj = {
+      notes:
+        (await action.getElementCount(this.notes)) > 0
+          ? await action.getText(this.notes)
+          : null,
+    };
+    return obj;
+  },
+
+  click_contentButton: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.contentButton);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " contentButton is clicked");
+      res = await require("./eBookContents.page").isInitialized();
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "contentButton is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_toolsButton: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.toolsButton);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " toolsButton is clicked");
+      res = await require("./eBookTools.page").isInitialized();
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "toolsButton is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_closeButton: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.closeButton);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " closeButton is clicked");
+      res = await require("./eBook.page").isInitialized();
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "closeButton is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_homeButton: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.homeButton);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " homeButton is clicked");
+      res = await require("./dashboard.page").isInitialized();
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "homeButton is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_cqaEbookEvolveDropdown: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.cqaEbookEvolveDropdown);
+    if (true == res) {
+      await logger.logInto(
+        await stackTrace.get(),
+        " cqaEbookEvolveDropdown is clicked"
+      );
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "cqaEbookEvolveDropdown is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_cqaTestEbookOnlyAssets: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.cqaTestEbookOnlyAssets);
+    if (true == res) {
+      await logger.logInto(
+        await stackTrace.get(),
+        " cqaTestEbookOnlyAssets is clicked"
+      );
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "cqaTestEbookOnlyAssets is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_notes: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.notes);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " notes is clicked");
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "notes is NOT clicked",
+        "error"
+      );
+    }
+    return res;
+  },
+
+  click_pageNumber: async function () {
+    await logger.logInto(await stackTrace.get());
+    var res;
+    res = await action.click(this.pageNumber);
+    if (true == res) {
+      await logger.logInto(await stackTrace.get(), " pageNumber is clicked");
+    } else {
+      await logger.logInto(
+        await stackTrace.get(),
+        res + "pageNumber is NOT clicked",
+        "error"
+      );
+    }
+    return res;
   },
 };
---
+++ testAutomation_v1.0/test/ExperienceApp/eBook.test.js
@@ -0,0 +1,93 @@
+"use strict";
+var eBook= require('../../pages/ExperienceApp/eBook.page.js');
+var sts;
+
+module.exports = {
+TST_EBOO_TC_1 :   async function () { 
+sts = await eBook.isInitialized();
+await assertion.assertEqual(sts.pageStatus, true, "eBook page status mismatch");
+},
+
+TST_EBOO_TC_2 :   async function (testdata) { 
+sts = await eBook.click_contentButton();
+await assertion.assertEqual(sts.pageStatus, true,"Page is not launched. ");
+
+},
+
+TST_EBOO_TC_3 :   async function (testdata) { 
+sts = await eBook.click_toolsButton();
+await assertion.assertEqual(sts.pageStatus, true,"Page is not launched. ");
+
+},
+
+TST_EBOO_TC_4 :   async function (testdata) { 
+sts = await eBook.click_closeButton();
+await assertion.assertEqual(sts.pageStatus, true,"Page is not launched. ");
+
+},
+
+TST_EBOO_TC_5 :   async function (testdata) { 
+sts = await eBook.click_homeButton();
+await assertion.assertEqual(sts.pageStatus, true,"Page is not launched. ");
+
+},
+
+TST_EBOO_TC_6 :   async function (testdata) { 
+sts = await eBook.click_cqaEbookEvolveDropdown();
+await assertion.assertEqual(sts, true,"cqaEbookEvolveDropdown are not Clicked");
+},
+
+TST_EBOO_TC_7 :   async function (testdata) { 
+sts = await eBook.click_cqaTestEbookOnlyAssets();
+await assertion.assertEqual(sts, true,"cqaTestEbookOnlyAssets are not Clicked");
+},
+
+TST_EBOO_TC_8 :   async function (testdata) { 
+sts = await eBook.click_notes();
+await assertion.assertEqual(sts, true,"notes are not Clicked");
+},
+
+TST_EBOO_TC_9 :   async function (testdata) { 
+sts = await eBook.click_pageNumber();
+await assertion.assertEqual(sts, true,"pageNumber are not Clicked");
+},
+
+TST_EBOO_TC_10 :   async function (testdata) { 
+sts = await eBook.getData_ebookEvolve(testdata);
+await assertion.assertEqual(sts.cqa_ebook_evolve, testdata.cqa_ebook_evolve,"cqa_ebook_evolve Values is not as expected.");
+},
+
+TST_EBOO_TC_11 :   async function (testdata) { 
+sts = await eBook.getData_eTextToolBar(testdata);
+await assertion.assertEqual(sts.contentButton, testdata.contentButton,"contentButton Values is not as expected.");
+await assertion.assertEqual(sts.toolsButton, testdata.toolsButton,"toolsButton Values is not as expected.");
+await assertion.assertEqual(sts.homeButton, testdata.homeButton,"homeButton Values is not as expected.");
+await assertion.assertEqual(sts.pageNumber, testdata.pageNumber,"pageNumber Values is not as expected.");
+},
+
+TST_EBOO_TC_12 :   async function (testdata) { 
+sts = await eBook.getData_ebookContents(testdata);
+await assertion.assertEqual(sts.closeButton, testdata.closeButton,"closeButton Values is not as expected.");
+},
+
+TST_EBOO_TC_13 :   async function (testdata) { 
+sts = await eBook.getData_ebookToolsNotes(testdata);
+await assertion.assertEqual(sts.myNotes, testdata.myNotes,"myNotes Values is not as expected.");
+},
+
+TST_EBOO_TC_14 :   async function (testdata) { 
+sts = await eBook.getData_ebookContent(testdata);
+await assertion.assertEqual(sts.cqaEbookEvolveDropdown, testdata.cqaEbookEvolveDropdown,"cqaEbookEvolveDropdown Values is not as expected.");
+},
+
+TST_EBOO_TC_15 :   async function (testdata) { 
+sts = await eBook.getData_ebookContentDropdown(testdata);
+await assertion.assertEqual(sts.cqaTestEbookOnlyAssets, testdata.cqaTestEbookOnlyAssets,"cqaTestEbookOnlyAssets Values is not as expected.");
+},
+
+TST_EBOO_TC_16 :   async function (testdata) { 
+sts = await eBook.getData_ebookTools(testdata);
+await assertion.assertEqual(sts.notes, testdata.notes,"notes Values is not as expected.");
+},
+
+}
