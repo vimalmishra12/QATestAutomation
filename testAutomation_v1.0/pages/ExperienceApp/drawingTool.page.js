9bf92bcc62c4a8506884c4f046cb0764cdc20fbf
vikrantaChaudhary <vikrant.chaudhary@comprotechnologies.com>
2024-11-06 12:56:54 +0530

    undo redo feature implemented

--- testAutomation_v1.0/pages/ExperienceApp/drawingTool.page.js
+++ testAutomation_v1.0/pages/ExperienceApp/drawingTool.page.js
@@ -4,14 +4,23 @@ var selectorFile = jsonParserUtil.jsonParser(selectorDir)
 var appShellPage = require('./appShell.page.js')
 
 module.exports = {
-drawingToolScribble: selectorFile.css.ComproC1.drawingTool.drawingToolScribble,
-drawingToolPenColour: selectorFile.css.ComproC1.drawingTool.drawingToolPenColour,
-drawingToolPenWidth: selectorFile.css.ComproC1.drawingTool.drawingToolPenWidth,
-drawingToolHighlighter: selectorFile.css.ComproC1.drawingTool.drawingToolHighlighter,
-drawingToolEraser: selectorFile.css.ComproC1.drawingTool.drawingToolEraser,
-drawingToolUndo: selectorFile.css.ComproC1.drawingTool.drawingToolUndo,
-drawingToolRedo: selectorFile.css.ComproC1.drawingTool.drawingToolRedo,
-drawingToolPresentation: selectorFile.css.ComproC1.drawingTool.drawingToolPresentation,
+  drawingToolScribble: selectorFile.css.ComproC1.drawingTool.drawingToolScribble,
+  drawingToolPenColour: selectorFile.css.ComproC1.drawingTool.drawingToolPenColour,
+  drawingToolPenWidth: selectorFile.css.ComproC1.drawingTool.drawingToolPenWidth,
+  drawingToolHighlighter: selectorFile.css.ComproC1.drawingTool.drawingToolHighlighter,
+  drawingToolEraser: selectorFile.css.ComproC1.drawingTool.drawingToolEraser,
+  drawingToolUndo: selectorFile.css.ComproC1.drawingTool.drawingToolUndo,
+  drawingToolRedo: selectorFile.css.ComproC1.drawingTool.drawingToolRedo,
+  drawingToolPresentation: selectorFile.css.ComproC1.drawingTool.drawingToolPresentation,
+  penColourGreen: selectorFile.css.ComproC1.drawingTool.penColourGreen,
+  penColourBlue: selectorFile.css.ComproC1.drawingTool.penColourBlue,
+  penColourRed: selectorFile.css.ComproC1.drawingTool.penColourRed,
+  penColourBlack: selectorFile.css.ComproC1.drawingTool.penColourBlack,
+  penStroke4: selectorFile.css.ComproC1.drawingTool.penStroke4,
+  penStroke3: selectorFile.css.ComproC1.drawingTool.penStroke3,
+  penStroke2: selectorFile.css.ComproC1.drawingTool.penStroke2,
+  penStroke1: selectorFile.css.ComproC1.drawingTool.penStroke1,
+  
 
 
 isInitialized: async function ()
@@ -38,9 +47,18 @@ drawingToolEraser:(( await action.getElementCount(this.drawingToolEraser)) > 0) 
 drawingToolUndo:(( await action.getElementCount(this.drawingToolUndo)) > 0) ? await action.getText(this.drawingToolUndo) : null,
 drawingToolRedo:(( await action.getElementCount(this.drawingToolRedo)) > 0) ? await action.getText(this.drawingToolRedo) : null,
 drawingToolPresentation:(( await action.getElementCount(this.drawingToolPresentation)) > 0) ? await action.getText(this.drawingToolPresentation) : null,
+penColourGreen:(( await action.getElementCount(this.penColourGreen)) > 0) ? await action.getText(this.penColourGreen) : null,
+penColourBlue:(( await action.getElementCount(this.penColourBlue)) > 0) ? await action.getText(this.penColourBlue) : null,
+penColourRed:(( await action.getElementCount(this.penColourRed)) > 0) ? await action.getText(this.penColourRed) : null,
+penColourBlack:(( await action.getElementCount(this.penColourBlack)) > 0) ? await action.getText(this.penColourBlack) : null,
+penStroke4:(( await action.getElementCount(this.penStroke4)) > 0) ? await action.getText(this.penStroke4) : null,
+penStroke3:(( await action.getElementCount(this.penStroke3)) > 0) ? await action.getText(this.penStroke3) : null,
+penStroke2:(( await action.getElementCount(this.penStroke2)) > 0) ? await action.getText(this.penStroke2) : null,
+penStroke1:(( await action.getElementCount(this.penStroke1)) > 0) ? await action.getText(this.penStroke1) : null,
 }
  return obj; 
 },
+
 
 
 click_drawingToolScribble: async function () {
@@ -134,6 +152,46 @@ await logger.logInto(await stackTrace.get(), res +"drawingToolScribble is NOT cl
 }
 return res;
 },
+
+click_drawingToolHighlighter: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.drawingToolHighlighter);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " drawingToolScribble is clicked");
+  
+   const canvasElement = await $(this.drawingToolPresentation); // Ensure the canvas selector is correct
+  
+   // Scroll the canvas into view, if necessary
+   await canvasElement.scrollIntoView();
+   
+   // Define drawing actions using performActions
+   await browser.performActions([
+     {
+       type: 'pointer',
+       id: 'mouse1',
+       parameters: { pointerType: 'mouse' },
+       actions: [
+        { type: 'pointerMove', origin: canvasElement, x: 100, y: 100}, // Starting point
+        { type: 'pointerDown', button: 0 },
+        { type: 'pointerMove', origin: canvasElement, x: 150, y: 150 }, // Draw to this point
+        { type: 'pointerMove', origin: canvasElement, x: 200, y: 200 }, // Continue drawing
+        { type: 'pointerMove', origin: canvasElement, x: 250, y: 250 }, // Ending point
+        { type: 'pointerUp', button: 0 } // Release mouse button
+      ]
+     }
+   ]);
+   
+   // Release all actions
+   await browser.releaseActions();
+   
+  
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"drawingToolScribble is NOT clicked", 'error');
+  }
+  return res;
+  },
 
 click_drawingToolPenColour: async function () {
 await logger.logInto(await stackTrace.get());
@@ -232,6 +290,9 @@ click_drawingToolPresentation: async function () {
 await logger.logInto(await stackTrace.get());
 var res;
 res =await action.click(this.drawingToolPresentation);
+
+console.log("vikrant 1",res);
+
 if (true == res) {
  await logger.logInto(await stackTrace.get(), " drawingToolPresentation is clicked");
 }
@@ -240,6 +301,114 @@ await logger.logInto(await stackTrace.get(), res +"drawingToolPresentation is NO
 }
 return res;
 },
+
+
+
+
+
+click_penColourGreen: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penColourGreen);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penColourGreen is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penColourGreen is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penColourBlue: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penColourBlue);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penColourBlue is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penColourBlue is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penColourRed: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penColourRed);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penColourRed is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penColourRed is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penColourBlack: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penColourBlack);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penColourBlack is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penColourBlack is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penStroke4: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penStroke4);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penStroke4 is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penStroke4 is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penStroke3: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penStroke3);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penStroke3 is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penStroke3 is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penStroke2: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penStroke2);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penStroke2 is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penStroke2 is NOT clicked", 'error');
+  }
+  return res;
+  },
+  
+  click_penStroke1: async function () {
+  await logger.logInto(await stackTrace.get());
+  var res;
+  res =await action.click(this.penStroke1);
+  if (true == res) {
+   await logger.logInto(await stackTrace.get(), " penStroke1 is clicked");
+  }
+  else {
+  await logger.logInto(await stackTrace.get(), res +"penStroke1 is NOT clicked", 'error');
+  }
+  return res;
+  }
 
 }
 
--- testAutomation_v1.0/test/ExperienceApp/drawingTool.test.js
+++ testAutomation_v1.0/test/ExperienceApp/drawingTool.test.js
@@ -38,16 +38,70 @@ sts = await drawingTool.click_drawingToolPresentation();
 await assertion.assertEqual(sts, true,"drawingToolPresentation are not Clicked");
 },
 
-TST_DRAW_TC_8 :   async function (testdata) { 
-sts = await drawingTool.getData_drawingTool(testdata);
-await assertion.assertEqual(sts.drawingToolScribble, testdata.drawingToolScribble,"drawingToolScribble Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolPenColour, testdata.drawingToolPenColour,"drawingToolPenColour Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolPenWidth, testdata.drawingToolPenWidth,"drawingToolPenWidth Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolHighlighter, testdata.drawingToolHighlighter,"drawingToolHighlighter Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolEraser, testdata.drawingToolEraser,"drawingToolEraser Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolUndo, testdata.drawingToolUndo,"drawingToolUndo Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolRedo, testdata.drawingToolRedo,"drawingToolRedo Values is not as expected.");
-await assertion.assertEqual(sts.drawingToolPresentation, testdata.drawingToolPresentation,"drawingToolPresentation Values is not as expected.");
-},
+TST_DRAW_TC_9 :   async function (testdata) { 
+    sts = await drawingTool.click_drawingToolHighlighter();
+    await assertion.assertEqual(sts, true,"drawingToolHighlighter are not Clicked");
+    },
+
+TST_DRAW_TC_10 :   async function (testdata) { 
+    sts = await drawingTool.click_penColourGreen();
+    await assertion.assertEqual(sts, true,"penColourGreen are not Clicked");
+    },
+    
+    TST_DRAW_TC_11 :   async function (testdata) { 
+    sts = await drawingTool.click_penColourBlue();
+    await assertion.assertEqual(sts, true,"penColourBlue are not Clicked");
+    },
+    
+    TST_DRAW_TC_12 :   async function (testdata) { 
+    sts = await drawingTool.click_penColourRed();
+    await assertion.assertEqual(sts, true,"penColourRed are not Clicked");
+    },
+    
+    TST_DRAW_TC_13 :   async function (testdata) { 
+    sts = await drawingTool.click_penColourBlack();
+    await assertion.assertEqual(sts, true,"penColourBlack are not Clicked");
+    },
+    
+    TST_DRAW_TC_14 :   async function (testdata) { 
+    sts = await drawingTool.click_penStroke4();
+    await assertion.assertEqual(sts, true,"penStroke4 are not Clicked");
+    },
+    
+    TST_DRAW_TC_15 :   async function (testdata) { 
+    sts = await drawingTool.click_penStroke3();
+    await assertion.assertEqual(sts, true,"penStroke3 are not Clicked");
+    },
+    
+    TST_DRAW_TC_16 :   async function (testdata) { 
+    sts = await drawingTool.click_penStroke2();
+    await assertion.assertEqual(sts, true,"penStroke2 are not Clicked");
+    },
+    
+    TST_DRAW_TC_17 :   async function (testdata) { 
+    sts = await drawingTool.click_penStroke1();
+    await assertion.assertEqual(sts, true,"penStroke1 are not Clicked");
+    },
+    
+    TST_DRAW_TC_8 :   async function (testdata) { 
+    sts = await drawingTool.getData_drawingTool(testdata);
+    await assertion.assertEqual(sts.drawingToolScribble, testdata.drawingToolScribble,"drawingToolScribble Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolPenColour, testdata.drawingToolPenColour,"drawingToolPenColour Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolPenWidth, testdata.drawingToolPenWidth,"drawingToolPenWidth Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolHighlighter, testdata.drawingToolHighlighter,"drawingToolHighlighter Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolEraser, testdata.drawingToolEraser,"drawingToolEraser Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolUndo, testdata.drawingToolUndo,"drawingToolUndo Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolRedo, testdata.drawingToolRedo,"drawingToolRedo Values is not as expected.");
+    await assertion.assertEqual(sts.drawingToolPresentation, testdata.drawingToolPresentation,"drawingToolPresentation Values is not as expected.");
+    await assertion.assertEqual(sts.penColourGreen, testdata.penColourGreen,"penColourGreen Values is not as expected.");
+    await assertion.assertEqual(sts.penColourBlue, testdata.penColourBlue,"penColourBlue Values is not as expected.");
+    await assertion.assertEqual(sts.penColourRed, testdata.penColourRed,"penColourRed Values is not as expected.");
+    await assertion.assertEqual(sts.penColourBlack, testdata.penColourBlack,"penColourBlack Values is not as expected.");
+    await assertion.assertEqual(sts.penStroke4, testdata.penStroke4,"penStroke4 Values is not as expected.");
+    await assertion.assertEqual(sts.penStroke3, testdata.penStroke3,"penStroke3 Values is not as expected.");
+    await assertion.assertEqual(sts.penStroke2, testdata.penStroke2,"penStroke2 Values is not as expected.");
+    await assertion.assertEqual(sts.penStroke1, testdata.penStroke1,"penStroke1 Values is not as expected.");
+    },
+        
 
 }
