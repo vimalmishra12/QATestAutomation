4adb3b235da39a7d6ec68d7c2636fc4e1b6e9e80
Hritik Jaiswal <fakehrj@gmail.com>
2025-05-26 15:58:05 +0530

    completed highlighter test with using highlighter button

--- testAutomation_v1.0/test/ExperienceApp/vhlhighlighter.test.js
+++ testAutomation_v1.0/test/ExperienceApp/vhlhighlighter.test.js
@@ -10,17 +10,22 @@ await assertion.assertEqual(sts, true,"highlighterIcon are not Clicked");
 
 TST_VHLH_TC_2 :   async function (testdata) { 
 sts = await vhlhighlighter.click_eraserButton();
-await assertion.assertEqual(sts, true,"eraserButton are not Clicked");
+await assertion.assertEqual(sts, true,"eraserButton is not Clicked");
 },
 
 TST_VHLH_TC_3 :   async function (testdata) { 
 sts = await vhlhighlighter.click_highlighterBtn();
-await assertion.assertEqual(sts, true,"highlighterBtn are not Clicked");
+await assertion.assertEqual(sts, true,"highlighterBtn is not Clicked");
 },
 
 TST_VHLH_TC_4 :   async function (testdata) { 
 sts = await vhlhighlighter.click_closeIcon();
-await assertion.assertEqual(sts, true,"closeIcon are not Clicked");
+await assertion.assertEqual(sts, true,"closeIcon is not Clicked");
 },
+
+TST_VHLH_TC_5 :   async function (testdata) { 
+sts = await vhlhighlighter.click_removeAllButton();
+await assertion.assertEqual(sts, true,"Remove all Button is not Clicked");
+}
 
 }
